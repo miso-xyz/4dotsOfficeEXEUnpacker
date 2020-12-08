38a8458d708aeb38c00c7357c6a7854cb33e181e
misonothx but as garfield <61099345+miso-xyz@users.noreply.github.com>
2020-12-08 13:35:15 +0000

    Add files via upload

---
+++ 4dotsExcelEXEUnpacker/Module1.vb
@@ -0,0 +1,57 @@
+﻿Module Module1
+    Dim Unpacker As New Unpacker
+    Sub Main(ByVal args() As String)
+        Console.Title = "4dots Excel to EXE Unpacker v1.0.0"
+        If args.Contains("--help") Then
+            Console.WriteLine("4dots Excel to EXE Unpacker v1.0.0 by misonothx | sinister.ly | miyako <3")
+            Console.WriteLine("Tested with Version 2.0")
+            Console.WriteLine()
+            Console.WriteLine("--help | Lists available arguments")
+            Console.WriteLine("--only_audio | only dumps the background audio")
+            Console.WriteLine("--only_images | only dumps images")
+            Console.WriteLine()
+            Console.WriteLine("PS: The actual Excel file cannot be dumped due to 4dots's packer taking screenshots of the file instead of actual giving the file")
+            Console.WriteLine()
+            Console.WriteLine("PS N°2: The version that the tool has been tested with actually isn't even able to include the screenshots it took of the file, the unpacker however can extract them yet isn't used")
+            Console.ReadKey()
+            End
+        End If
+        Console.WriteLine("4dots Excel to EXE Unpacker v1.0.0 by misonothx | sinister.ly | miyako <3")
+        Console.Write("arguments given: ")
+        If args.Length = 0 Then
+            Console.Write("nothing")
+        Else
+            For x = 0 To args.Count - 1
+                If IO.File.Exists(args(x)) Then
+                    Console.Write("%input_file%")
+                End If
+            Next
+        End If
+        Console.WriteLine()
+        Console.WriteLine()
+        For x = 0 To args.Count - 1
+            If IO.File.Exists(args(x)) Then
+                Try
+                    Unpacker.asm = System.Reflection.Assembly.LoadFile(args(x))
+                Catch ex As Exception
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("The input file isn't a valid assembly file!")
+                    Console.ReadKey()
+                    End
+                End Try
+                Unpacker.asm_name = IO.Path.GetFileName(args(x))
+                If args.Contains("--only_audio") Then
+                    Unpacker.only_audio = True
+                ElseIf args.Contains("--only_img") Then
+                    Unpacker.only_img = True
+                End If
+                Unpacker.Extract()
+            End If
+        Next
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("No input files given!")
+        Console.ReadKey()
+        End
+    End Sub
+
+End Module
---
+++ 4dotsExcelEXEUnpacker/Unpacker.vb
@@ -0,0 +1,193 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.Reflection
+Imports System.Xml
+Imports System.Drawing
+
+Public Class Unpacker
+    Private prj As New XmlDocument()
+    Private prj_node As XmlNode = prj.SelectSingleNode("//Misc")
+    Public asm As Assembly
+    Public asm_name As String
+    Private ext_path As String
+    Public only_audio As Boolean = False
+    Public only_img As Boolean = False
+
+    Public Function CryptedImages() As Boolean
+        Return (prj_node.Attributes.GetNamedItem("EncryptImages").Value.ToString() = Boolean.TrueString)
+    End Function
+
+    Public Sub LoadXMLProjectInMemory()
+        Using binaryReader As BinaryReader = New BinaryReader(asm.GetManifestResourceStream("EXESlideshow.project.xml"))
+            Using memoryStream As MemoryStream = New MemoryStream()
+                While True
+                    Dim num As Long = 32768L
+                    Dim buffer As Byte() = New Byte(num - 1) {}
+                    Dim num2 As Integer = binaryReader.Read(buffer, 0, CInt(num))
+                    If num2 <= 0 Then
+                        Exit While
+                    End If
+                    memoryStream.Write(buffer, 0, num2)
+                End While
+                prj.LoadXml(Encoding.[Default].GetString(memoryStream.ToArray()))
+            End Using
+        End Using
+    End Sub
+    Public Function GetPassword()
+        If prj_node.Attributes.GetNamedItem("AskForPasswordValue").Value.ToString() <> Nothing Then
+            Return DecryptString(prj_node.Attributes.GetNamedItem("AskForPasswordValue").Value.ToString(), "493589549485043859430889230823")
+        End If
+        Return ""
+    End Function
+
+    Public Shared Function DecryptString(ByVal Message As String, ByVal Passphrase As String) As String
+        Dim utf8Encoding As UTF8Encoding = New UTF8Encoding()
+        Dim md5CryptoServiceProvider As MD5CryptoServiceProvider = New MD5CryptoServiceProvider()
+        Dim key As Byte() = md5CryptoServiceProvider.ComputeHash(utf8Encoding.GetBytes(Passphrase))
+        Dim tripleDESCryptoServiceProvider As TripleDESCryptoServiceProvider = New TripleDESCryptoServiceProvider()
+        tripleDESCryptoServiceProvider.Key = key
+        tripleDESCryptoServiceProvider.Mode = CipherMode.ECB
+        tripleDESCryptoServiceProvider.Padding = PaddingMode.PKCS7
+        Dim array As Byte() = Convert.FromBase64String(Message)
+        Dim bytes As Byte()
+        Try
+            Dim cryptoTransform As ICryptoTransform = tripleDESCryptoServiceProvider.CreateDecryptor()
+            bytes = cryptoTransform.TransformFinalBlock(array, 0, array.Length)
+        Finally
+            tripleDESCryptoServiceProvider.Clear()
+            md5CryptoServiceProvider.Clear()
+        End Try
+        Return utf8Encoding.GetString(bytes)
+    End Function
+
+    Public Shared Function IsImage(ByVal fp As String) As Boolean
+        fp = fp.ToLower()
+        Dim text As String = "*.png;*.bmp;*.gif;*.jpg;*.jpeg;*.bmp;*.tiff;"
+        Dim num As Integer = fp.LastIndexOf(".")
+        Dim result As Boolean
+        If num < 0 Then
+            result = False
+        Else
+            Dim str As String = fp.Substring(num)
+            result = (text.IndexOf("*" + str + ";") >= 0)
+        End If
+        Return result
+    End Function
+
+    Public Shared Function DecryptBytes(ByVal bMessage As Byte(), ByVal Passphrase As String) As Byte()
+        Dim utf8Encoding As UTF8Encoding = New UTF8Encoding()
+        Dim md5CryptoServiceProvider As MD5CryptoServiceProvider = New MD5CryptoServiceProvider()
+        Dim key As Byte() = md5CryptoServiceProvider.ComputeHash(utf8Encoding.GetBytes(Passphrase))
+        Dim tripleDESCryptoServiceProvider As TripleDESCryptoServiceProvider = New TripleDESCryptoServiceProvider()
+        tripleDESCryptoServiceProvider.Key = key
+        tripleDESCryptoServiceProvider.Mode = CipherMode.ECB
+        tripleDESCryptoServiceProvider.Padding = PaddingMode.PKCS7
+        Dim result As Byte()
+        Try
+            Dim cryptoTransform As ICryptoTransform = tripleDESCryptoServiceProvider.CreateDecryptor()
+            result = cryptoTransform.TransformFinalBlock(bMessage, 0, bMessage.Length)
+        Finally
+            tripleDESCryptoServiceProvider.Clear()
+            md5CryptoServiceProvider.Clear()
+        End Try
+        Return result
+    End Function
+
+    Public Sub Extract()
+        Dim audio_dmp As Boolean = False
+        ext_path = My.Application.Info.DirectoryPath & "/4dotsExcelEXEUnpacker/" & asm_name & "/"
+        IO.Directory.CreateDirectory(ext_path)
+        Dim img_int As Integer = 0
+        Dim manifestResourceNames As String() = asm.GetManifestResourceNames()
+        For i As Integer = 2 To manifestResourceNames.Length - 1
+            If manifestResourceNames(i).IndexOf("project.xml") >= 0 Then
+                LoadXMLProjectInMemory()
+            Else
+                Using binaryReader As BinaryReader = New BinaryReader(asm.GetManifestResourceStream(manifestResourceNames(i)))
+                    Using memoryStream As MemoryStream = New MemoryStream()
+                        While True
+                            Dim num As Long = 32768L
+                            Dim buffer As Byte() = New Byte(num - 1) {}
+                            Dim num2 As Integer = binaryReader.Read(buffer, 0, CInt(num))
+                            If num2 <= 0 Then
+                                Exit While
+                            End If
+                            memoryStream.Write(buffer, 0, num2)
+                        End While
+                        If manifestResourceNames(i).IndexOf("4dotsAudio") >= 0 Then
+                            If only_img Then
+                                Console.ForegroundColor = ConsoleColor.Yellow
+                                Console.WriteLine("Found Background Audio, skipping...")
+                                Console.ResetColor()
+                                Continue For
+                            End If
+                            Console.ForegroundColor = ConsoleColor.Green
+                            Console.WriteLine("Found Background Audio File, setting it in memory to dump!")
+                            Dim text As String = Path.GetTempFileName() + ".wav"
+                            File.WriteAllBytes(text, memoryStream.ToArray())
+                            Dim text2 As String = manifestResourceNames(i).Substring(manifestResourceNames(i).IndexOf("4dotsAudio"))
+                            If text2.IndexOf("4dotsAudioBackgroundMusic") >= 0 Then
+                                Console.ForegroundColor = ConsoleColor.Yellow
+                                Console.WriteLine("dumping Background Audio File...")
+                                Using FileStream As FileStream = File.Create(ext_path & "background-music.wav")
+                                    asm.GetManifestResourceStream(manifestResourceNames(i)).CopyTo(FileStream)
+                                End Using
+                                Console.ForegroundColor = ConsoleColor.Green
+                                Console.WriteLine("Background Audio File dumped!")
+                                Console.ResetColor()
+                                audio_dmp = True
+                                If only_audio Then
+                                    Return
+                                End If
+                            End If
+                        ElseIf IsImage(manifestResourceNames(i)) Then
+                            If only_audio Then
+                                Console.ForegroundColor = ConsoleColor.Yellow
+                                Console.WriteLine("Found Image, skipping...")
+                                Console.ResetColor()
+                                Continue For
+                            End If
+                            Try
+                                If CryptedImages() Then
+                                    Console.ForegroundColor = ConsoleColor.Green
+                                    Console.WriteLine("Found Image N°" & img_int & "! (Crypted), decrypting...")
+                                    Dim buffer2 As Byte() = DecryptBytes(memoryStream.ToArray(), "433424234234-93435849839453")
+                                    'File.WriteAllBytes(manifestResourceNames(i), buffer2)
+                                    Dim stream As MemoryStream = New MemoryStream(buffer2)
+                                    Dim item As Image = Image.FromStream(stream)
+                                    Console.ForegroundColor = ConsoleColor.Yellow
+                                    Console.WriteLine("Dumping image n°" & img_int & "...")
+                                    item.Save(ext_path & manifestResourceNames(i), System.Drawing.Imaging.ImageFormat.Png)
+                                    Console.WriteLine("Image N°" & img_int & " successfully dumped!")
+                                    Console.ResetColor()
+                                    img_int += 1
+                                Else
+                                    Console.ForegroundColor = ConsoleColor.Green
+                                    Console.WriteLine("Found Image N°" & img_int & "! (Not Crypted), decrypting...")
+                                    Dim item As Image = Image.FromStream(memoryStream)
+                                    Console.ForegroundColor = ConsoleColor.Yellow
+                                    Console.WriteLine("Dumping image n°" & img_int & "...")
+                                    item.Save(ext_path & manifestResourceNames(i), System.Drawing.Imaging.ImageFormat.Png)
+                                    Console.ForegroundColor = ConsoleColor.Green
+                                    Console.WriteLine("Image N°" & img_int & " successfully dumped!")
+                                    Console.ResetColor()
+                                    img_int += 1
+                                End If
+                            Catch
+                            End Try
+                        End If
+                    End Using
+                End Using
+            End If
+        Next
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.Write("Done! (dumped ")
+        If audio_dmp Then
+            Console.Write("background audio & ")
+        End If
+        Console.Write(img_int & " image(s))")
+        Console.ReadKey()
+        End
+    End Sub
+End Class
